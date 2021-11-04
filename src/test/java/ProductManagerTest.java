906f5e2b68c116c3b9105461fc8c06096297eeef
KarpovaO <grose@yandex.ru>
2021-11-05 00:34:10 +0300

    Product Manager update

--- src/test/java/ProductManagerTest.java
+++ src/test/java/ProductManagerTest.java
@@ -2,21 +2,21 @@ import domain.Book;
 import domain.Smartphone;
 import domain.Product;
 import manager.ProductManager;
+import repository.Repository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import repository.Repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ProductManagerTest {
+public class ProductManagerTest {
     private final ProductManager manager = new ProductManager((new Repository()));
-    private Product first = new Product(124, " toy", 350);
-    private Book second = new Book(132, "Math for kids", 600, "Иванов");
-    private Smartphone third = new Smartphone(135, "S20", 70000, "samsung");
-    private Book fourth = new Book(136, "S20", 230, "Петров");
+    private final Product first = new Product(124, " toy", 350);
+    private final Book second = new Book(132, "Math for kids", 600, "Иванов");
+    private final Smartphone third = new Smartphone(135, "S20", 70000, "samsung");
+    private final Book fourth = new Book(136, "S20", 230, "Петров");
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         manager.add(first);
         manager.add(second);
         manager.add(third);
@@ -24,32 +24,32 @@ class ProductManagerTest {
     }
 
     @Test
-    void shouldSearchByName() {
+    public void shouldSearchByName() {
         Product[] expected = {first};
         assertArrayEquals(expected, manager.searchBy("toy"));
 
     }
 
     @Test
-    void shouldSearchByAuthor() {
+    public void shouldSearchByAuthor() {
         Product[] expected = {second};
         assertArrayEquals(expected, manager.searchBy("Иванов"));
     }
 
     @Test
-    void shouldSearchByManufacturer() {
+    public void shouldSearchByManufacturer() {
         Product[] expected = {third};
         assertArrayEquals(expected, manager.searchBy("samsung"));
     }
 
     @Test
-    void shouldSearchByUnsuitableProduct() {
+    public void shouldSearchByUnsuitableProduct() {
         Product[] expected = {};
         assertArrayEquals(expected, manager.searchBy("nokia"));
     }
 
     @Test
-    void shouldSearchSameNameProduct() {
+    public void shouldSearchSameNameProduct() {
         Product[] expected = {third, fourth};
         assertArrayEquals(expected, manager.searchBy("S20"));
 
